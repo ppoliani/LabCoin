a0aa0ca43a6fcc13ddc15b0606a4a32ab7604278
ppoliani <ppoliani@gmail.com>
2017-11-18 11:14:19 +0000

    add toke actions

---
+++ src/js/ui/src/data/token/tokenActions.js
@@ -0,0 +1,30 @@
+import {createAction} from 'redux-actions'
+import {Map} from 'immutable'
+import fetch, {constructUrl} from '../../services/api'
+import {partial} from '../../services/fn'
+
+export const PURCHASE_TOKENS = 'TOKEN::PURCHASE_TOKENS';
+export const GET_TOKEN_BALANCE = 'TOKEN::GET_TOKEN_BALANCE';
+
+const TOKEN_URL = 'http://localhost:5000/tokens';
+
+export const purchaseTokensRoot = fetch => {
+  return createAction(
+    PURCHASE_TOKENS,
+    partial(fetch, 'POST', PROOF_URL)
+  );
+}
+
+export const getBalanceRoot = fetch => {
+  const getUrl = pubKey => constructUrl(TOKEN_URL, Map({pubKey}));
+  const fetchData = (fetch) ['∘'] (getUrl);
+
+  return createAction(
+    GET_TOKEN_BALANCE,
+    fetchData
+  );
+}
+
+
+export const purchaseTokens = partial(purchaseTokensRoot, fetch)
+export const getBalanceRoot = partial(getBalance, fetch)
