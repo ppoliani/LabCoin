5cd9efe95c0d0494151a0b0cd52ad0c21de47388
ppoliani <ppoliani@gmail.com>
2017-11-19 12:38:16 +0000

    test the transfer method

--- src/solidity/LabCoin/test/labCoinERC20.js
+++ src/solidity/LabCoin/test/labCoinERC20.js
@@ -1,11 +1,52 @@
 const LabCoin = artifacts.require('./LabCoinERC20.sol');
 
 contract('LabCoinERC20', accounts => {
-  it('Should put 1000000 labcoins to the first account', () => {
+  it('should put 1000000 labcoins to the first account', () => {
     return LabCoin.deployed()
     .then(contractInst => contractInst.balanceOf(accounts[0]))
     .then(balance => {
       assert.equal(balance.valueOf(), 100000000, 'Wrong initial supply')
     })
   });
+
+  it('should make the totalSupply available', () => {
+    return LabCoin.deployed()
+      .then(contractInst => contractInst.totalSupply())
+      .then(supply => {
+        assert.equal(supply.valueOf(), 100000000, 'Wrong initial supply')
+      })
+  });
+
+  it('should transfer an amount of token from the sender to the recipient of a transaction', () => {
+    const sender = accounts[0];
+    const recipient = accounts[1];
+    const amount = 1000;
+    let senderStartingBalance;
+    let recipientStartingBalance;
+    let contractInst;
+
+    return LabCoin.deployed()
+      .then(inst => {
+        contractInst = inst;
+
+        return Promise.all([
+          contractInst.balanceOf(sender),
+          contractInst.balanceOf(recipient),
+          contractInst.transfer(recipient, amount, {from: sender}),
+        ]);
+      })
+      .then(([_senderStartingBalance, _recipientStartingBalance]) => {
+        senderStartingBalance = _senderStartingBalance;
+        recipientStartingBalance = _recipientStartingBalance;
+
+        return Promise.all([
+          contractInst.balanceOf(sender),
+          contractInst.balanceOf(recipient)
+        ]);
+      })
+      .then(([senderCurrentBalance, recipientCurrentBalance]) => {
+        assert.equal(senderStartingBalance.valueOf() - amount, senderCurrentBalance.valueOf(), 'sender has worng balance after the transfer');
+        assert.equal(Number(recipientStartingBalance.valueOf()) + amount, recipientCurrentBalance.valueOf(), 'recipient has worng balance after the transfer');
+      });
+  }); 
 });
