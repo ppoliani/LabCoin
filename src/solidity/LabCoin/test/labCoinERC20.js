66131cdf67378caa1547a1b4fd3b369859f70f69
ppoliani <ppoliani@gmail.com>
2017-11-19 12:56:41 +0000

    use async/ await

--- src/solidity/LabCoin/test/labCoinERC20.js
+++ src/solidity/LabCoin/test/labCoinERC20.js
@@ -1,71 +1,51 @@
 const LabCoin = artifacts.require('./LabCoinERC20.sol');
 
 contract('LabCoinERC20', accounts => {
-  it('should put 1000000 labcoins to the first account', () => {
-    return LabCoin.deployed()
-    .then(contractInst => contractInst.balanceOf(accounts[0]))
-    .then(balance => {
-      assert.equal(balance.valueOf(), 100000000, 'Wrong initial supply')
-    })
+  it('should put 1000000 labcoins to the first account', async () => {
+    const contractInst = await LabCoin.deployed();
+    const balance = await contractInst.balanceOf(accounts[0]);
+
+    assert.equal(balance.valueOf(), 100000000, 'Wrong initial supply')
   });
 
-  it('should make the totalSupply available', () => {
-    return LabCoin.deployed()
-      .then(contractInst => contractInst.totalSupply())
-      .then(supply => {
-        assert.equal(supply.valueOf(), 100000000, 'Wrong initial supply')
-      })
+  it('should make the totalSupply available', async () => {
+    const contractInst = await LabCoin.deployed();
+    const supply = await contractInst.totalSupply();
+    
+    assert.equal(supply.valueOf(), 100000000, 'Wrong initial supply')
   });
 
-  it('should transfer an amount of token from the sender to the recipient of a transaction', () => {
+  it('should transfer an amount of token from the sender to the recipient of a transaction', async () => {
+    const amount = 1000;
     const sender = accounts[0];
     const recipient = accounts[1];
-    const amount = 1000;
-    let senderStartingBalance;
-    let recipientStartingBalance;
-    let contractInst;
+    const contractInst = await LabCoin.deployed();
+    const senderStartingBalance = await contractInst.balanceOf(sender);
+    const recipientStartingBalance = await contractInst.balanceOf(recipient);
+    const transferTxnResult = await contractInst.transfer(recipient, amount, {from: sender});
+    const senderCurrentBalance = await contractInst.balanceOf(sender);
+    const recipientCurrentBalance = await contractInst.balanceOf(recipient);
 
-    return LabCoin.deployed()
-      .then(inst => {
-        contractInst = inst;
-
-        return Promise.all([
-          contractInst.balanceOf(sender),
-          contractInst.balanceOf(recipient),
-          contractInst.transfer(recipient, amount, {from: sender}),
-        ]);
-      })
-      .then(([_senderStartingBalance, _recipientStartingBalance]) => {
-        senderStartingBalance = _senderStartingBalance;
-        recipientStartingBalance = _recipientStartingBalance;
-
-        return Promise.all([
-          contractInst.balanceOf(sender),
-          contractInst.balanceOf(recipient)
-        ]);
-      })
-      .then(([senderCurrentBalance, recipientCurrentBalance]) => {
-        assert.equal(senderStartingBalance.valueOf() - amount, senderCurrentBalance.valueOf(), 'sender has worng balance after the transfer');
-        assert.equal(Number(recipientStartingBalance.valueOf()) + amount, recipientCurrentBalance.valueOf(), 'recipient has worng balance after the transfer');
-      });
+    assert.equal(senderStartingBalance.valueOf() - amount, senderCurrentBalance.valueOf(), 'sender has worng balance after the transfer');
+    assert.equal(Number(recipientStartingBalance.valueOf()) + amount, recipientCurrentBalance.valueOf(), 'recipient has worng balance after the transfer');
   }); 
 
-  it('should set the ending time of the ICO', () => {
+  it('should set the ending time of the ICO', async () => {
     const now = Number(new Date());
     let nextSecond = new Date();
     nextSecond.setSeconds(nextSecond.getSeconds() + 2);
     nextSecond = Number(nextSecond);
 
-    return LabCoin.deployed()
-      .then(contractInst => {
-        return Promise.all([
-          contractInst.setEndtime(nextSecond),
-          contractInst.endTime()
-        ]);
-      })
-      .then(([result, endtime]) => {
-        assert.ok(result);
-        assert.equal(nextSecond, endtime.valueOf(), 'Failed to update the endtime')
-      })
+    const contractInst = await LabCoin.deployed();
+    const result = await contractInst.setEndtime(nextSecond);
+    const endtime = await contractInst.endTime();
+
+    assert.ok(result);
+    assert.equal(nextSecond, endtime.valueOf(), 'Failed to update the endtime')
   });
+
+  // it('should increase the supply of tokens when someone is participating in the ICO', async () => {
+  //   const constractInst = LabCoin.deployed();
+
+  // });
 });
